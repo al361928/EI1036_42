7749aae77997804f4d791fbff6db90fd3eebed1e
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.com>
2020-11-23 06:35:37 -0500

    caja flotante

---
+++ Lab/P2_proy/js/caja.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "felx";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+})
